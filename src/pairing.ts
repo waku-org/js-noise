dc2fd56797bc7c33d12fbbff562e8ae37699aecf
Emil Ivanichkov <emil.ivanichkov@gmail.com>
2024-02-15 11:59:42 +0200

    refactor: Use `log` instead of `console.log` in `pairing` module

--- src/pairing.ts
+++ src/pairing.ts
@@ -247,7 +247,7 @@ export class WakuPairing {
     const confirmationPromise = this.isAuthCodeConfirmed();
     await delay(100);
     this.eventEmitter.emit("authCodeGenerated", this.handshake.genAuthcode());
-    console.log("Waiting for authcode confirmation...");
+    log("Waiting for authcode confirmation...");
     const confirmed = await confirmationPromise;
     if (!confirmed) {
       throw new Error("authcode is not confirmed");
@@ -309,7 +309,7 @@ export class WakuPairing {
     const confirmationPromise = this.isAuthCodeConfirmed();
     await delay(100);
     this.eventEmitter.emit("authCodeGenerated", this.handshake.genAuthcode());
-    console.log("Waiting for authcode confirmation...");
+    log("Waiting for authcode confirmation...");
     const confirmed = await confirmationPromise;
     if (!confirmed) {
       throw new Error("authcode is not confirmed");
